33a938427fec1c64bf15745aa53eec83b9f8f6ac
M. Burak Ovur <111775102+mburakovur@users.noreply.github.com>
2023-04-13 13:34:44 +0300

    collectionwork3 folder added

---
+++ collectionwork3/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.Write("Lutfen bir cumle giriniz: ");
+    string? s = Console.ReadLine();
+    s = s.ToLower();
+
+    ArrayList list = new ArrayList();
+    foreach(var item in s) 
+    {
+        if (item == 'a' || item == 'e' || item == 'ı' || item == 'i' || item == 'o' || item == 'ö'|| item == 'u' || item == 'ü')
+        {
+            list.Add(item);
+        }
+    }
+        
+    Console.WriteLine("Dizinin sesli elemanlari: ");
+    foreach(var item in list)
+        Console.Write(item + "-");
